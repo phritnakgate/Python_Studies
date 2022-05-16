2e5a80476edcea8e4efbaeb52a34f5ad2b0b72b8
Phrit Nakgate <boss5500216@gmail.com>
2022-05-16 14:19:18 +0700

    Delete OOP_Python directory

--- OOP_Python/Class แยกไฟล์/Sale.py
+++
@@ -1,9 +0,0 @@
-from Employee import Employee
-class Sale(Employee):
-    __departmentName = 'แผนกขายสินค้า'
-    def __init__(self,name,salary,area):
-        super().__init__(name,salary,self.__departmentName)
-        self.area = area
-    def _showData(self):
-        super()._showData()
-        print('พื้นที่ทำงาน: ' + self.area)
